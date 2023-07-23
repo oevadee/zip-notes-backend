586b84e9001520bd9ef675754b29a962dfb09b22
oveadee <a.szczechura.98@gmail.com>
2023-07-24 01:20:32 +0200

    feat: add essential helpers: clock, incrementIdGenerator, NotFoundError

---
+++ src/api/clock.ts
@@ -0,0 +1,3 @@
+export type Clock = () => Date;
+
+export const clock = () => new Date();
---
+++ src/api/incrementIdGenerator.ts
@@ -0,0 +1,6 @@
+export type IdGenerator = () => string;
+
+export const incrementIdGenerator = <T>(formatter: (n: number) => T) => {
+  let id = 0;
+  return () => formatter(id++);
+};
